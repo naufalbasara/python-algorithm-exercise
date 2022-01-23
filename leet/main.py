4a078891c11671de1690763afdf0dda1c63e6d29
naufalbasara <naufalbasara@outlook.com>
2022-01-23 17:18:25 +0700

    add 2 problems

--- leet/main.py
+++ leet/main.py
@@ -1,3 +1,19 @@
+from math import ceil, floor
+
+# linear search
+def linearSearch(array, target): #memory issues
+    for i in range(len(array)):
+        if array[i] == target:
+            return i
+    return -1
+# recursive linear search
+def recursiveLinearSearch(array, target, start):
+    if array[start] == target:
+        return start
+    else:
+        return recursiveLinearSearch(array, target, start+1)
+
+
 # recursive binary search
 def recursiveBinarySearch(array, target, left, right):
     try:
@@ -10,7 +26,7 @@ def recursiveBinarySearch(array, target, left, right):
             return recursiveBinarySearch(array, target, left, center-1)
     except:
         return "{target} was not found in the array".format(target = target)
-
+# loop binary search
 def binarySearch(array, target):
     left, right = 0, len(array)-1
     while left <= right:
@@ -24,10 +40,51 @@ def binarySearch(array, target):
     return -1
 
 
-def search(array, target):
+def search(array, target, type):
     array.sort()
-    return binarySearch(array, target)
+    if type.lower() == "linear":
+        return linearSearch(array, target)
+    elif type.lower() == "binary":
+        return binarySearch(array, target)
 
+# First Loop Bad Version
+def firstBadVersion(n):
+    #isBadVersion() function is and API fetch through Leet Website 
+    start = 1
+    end = n
 
-array = [-1,0,3,5,9,12]
-print(search(array, 100))
+    while start <= end:
+        mid = (start+end)//2
+        if isBadVersion(mid) and not isBadVersion(mid-1): #if bad version and the previous is not bad, then its the first
+            return mid
+        elif not isBadVersion(mid): #if not bad version, check the rest next number
+            start = mid + 1
+        else: #if bad version and the previous is bad, then check previous
+            end = mid - 1
+
+# search insert position
+def searchInsert(array, target):
+    left = 0
+    right = len(array)-1
+
+    while left <= right:
+        mid = (left + right)//2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            right = mid-1
+        else:
+            left = mid+1
+    return left
+
+def recursiveSearchInsert(array, target, left, right):
+    try:
+        center = int((left + right)/2)
+        if array[center] == target:
+            return "{target} is in index {center} of the array".format(target = target, center=center)
+        elif array[center] < target:
+            return recursiveSearchInsert(array, target, center + 1, right)
+        else:
+            return recursiveSearchInsert(array, target, left, center-1)
+    except:
+        return left
