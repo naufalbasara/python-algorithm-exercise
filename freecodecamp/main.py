bd099c5e1f39fc378b7d34637869c50265dcb887
naufalbasara <naufalbasara@outlook.com>
2023-01-19 01:56:19 +0700

    Shape calculator freecodecamp project

--- freecodecamp/main.py
+++ freecodecamp/main.py
@@ -1,5 +1,6 @@
 from arithmetic_arrange import arithmetic_arranger
 from time_calculator import add_time
+import shape_calculator
 
 if __name__ == '__main__':
     # arithmetic arranger
@@ -9,5 +10,27 @@ if __name__ == '__main__':
         problems.append(problem_input)
         problem_input = input("Formatting will be number1 + number2, example: 1 + 2\nInput problems to solve:")
 
+    # time calculator
+    print(add_time("6:30 PM", "205:12"))
+    print()
+    print(round(212/60))
 
-    print(arithmetic_arranger(problems, True))
+    # Polygon Area Calculator
+    rect = shape_calculator.Rectangle(10, 5)
+    print(rect.get_area())
+    rect.set_height(2)
+    rect.set_width(6)
+    print(rect.get_perimeter())
+    print(rect)
+    print(rect.get_picture())
+
+    sq = shape_calculator.Square(9)
+    print(sq.get_area())
+    sq.set_side(3)
+    print(sq.get_diagonal())
+    print(sq.get_picture())
+
+    rect1 = shape_calculator.Rectangle(10, 10)
+    rect2 = shape_calculator.Rectangle(4, 8)
+    print(rect2.get_amount_inside(rect1))
+    
---
+++ freecodecamp/shape_calculator.py
@@ -0,0 +1,66 @@
+class Rectangle(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
+    
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2*(self.width+self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        picture = ""
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        for i in range(self.height):
+            for j in range(self.width):
+                picture += '*'
+                if j == self.width-1:
+                    picture += '\n'
+        return picture
+    
+    def get_amount_inside(self, object):
+        if self.get_area() < object.get_area():
+            return 0
+        elif self.get_area() == object.get_area():
+            return 1
+        return int(self.get_area()/object.get_area())
+
+class Square(Rectangle):
+    def __init__(self, side):
+        self.width = side
+        self.height = side
+    
+    def __str__(self):
+        return f'Square(side={self.width})'
+    
+    def set_width(self, width):
+        self.width = width
+        self.height = width
+
+    def set_height(self, height):
+        self.width = height
+        self.height = height
+        
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+    
+    def get_area(self):
+        return self.width**2
+
+    def get_perimeter(self):
+        return 4*(self.width)
